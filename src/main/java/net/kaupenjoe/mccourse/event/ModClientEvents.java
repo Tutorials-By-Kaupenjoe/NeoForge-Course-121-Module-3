73734ce04d2a67319876f5e35832796dd340ad8e
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-07-09 16:13:32 +0200

    events

---
+++ src/main/java/net/kaupenjoe/mccourse/event/ModClientEvents.java
@@ -0,0 +1,27 @@
+package net.kaupenjoe.mccourse.event;
+
+import net.kaupenjoe.mccourse.MCCourseMod;
+import net.kaupenjoe.mccourse.item.ModItems;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ComputeFovModifierEvent;
+
+@EventBusSubscriber(modid = MCCourseMod.MOD_ID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
+public class ModClientEvents {
+    @SubscribeEvent
+    public static void onComputeFovModifierEvent(ComputeFovModifierEvent event) {
+        if(event.getPlayer().isUsingItem() && event.getPlayer().getUseItem().getItem() == ModItems.KAUPEN_BOW.get()) {
+            float fovModifier = 1f;
+            int ticksUsingItem = event.getPlayer().getTicksUsingItem();
+            float deltaTicks = (float)ticksUsingItem / 20f;
+            if(deltaTicks > 1f) {
+                deltaTicks = 1f;
+            } else {
+                deltaTicks *= deltaTicks;
+            }
+            fovModifier *= 1f - deltaTicks * 0.15f;
+            event.setNewFovModifier(fovModifier);
+        }
+    }
+}
---
+++ src/main/java/net/kaupenjoe/mccourse/event/ModEvents.java
@@ -0,0 +1,39 @@
+package net.kaupenjoe.mccourse.event;
+
+import net.kaupenjoe.mccourse.MCCourseMod;
+import net.kaupenjoe.mccourse.item.ModItems;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+
+@EventBusSubscriber(modid = MCCourseMod.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
+public class ModEvents {
+    @SubscribeEvent
+    public static void livingDamage(LivingDamageEvent.Pre event) {
+        if(event.getEntity() instanceof Sheep sheep) {
+            if(event.getSource().getDirectEntity() instanceof Player player) {
+                if(player.getMainHandItem().getItem() == ModItems.METAL_DETECTOR.get()) {
+                    player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a freaking Sheep with a Metal Detector!"));
+                }
+
+                if(player.getMainHandItem().getItem() == ModItems.TOMATO.get()) {
+                    player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a freaking Sheep with a tomato!"));
+                    sheep.addEffect(new MobEffectInstance(MobEffects.JUMP, 600, 50));
+                    player.getMainHandItem().shrink(1);
+                }
+
+                if(player.getMainHandItem().getItem() == Items.END_ROD) {
+                    player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a freaking Sheep with AN END ROD WHAT?!!"));
+                    sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 600, 50));
+                    player.getMainHandItem().shrink(1);
+                }
+            }
+        }
+    }
+}
